9851c03ad978f36c0a824090b5ecd69036aa9add
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-12 14:15:57 -0500

    Update Controller.php
    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -41,7 +41,7 @@ $message = $css . "
       <table style='background:red' border='0' cellspacing='0' cellpadding='0'>
           <tr>
               <td style='vertical-align: top;'>
-                  <img src='https://lanyardsforyou.com/Pages/General/Menu/Logo.png' alt='Imagen' style='display: block; margin-right: 10px; width: 80px;'>
+                  <img src='https://lanyardsforyou.com/Pages/General/Menu/Logo.png' alt='Imagen' style='display: block; margin-right: 10px; width: 120px;'>
               </td>
               <td style='vertical-align: top;'>
                   <h1>Título de la tabla</h1>
