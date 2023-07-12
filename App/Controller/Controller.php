645a8059709ceae8de19b089aca8b881f78c20ab
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-12 05:26:28 -0500

    Update Controller.php
    ,,,

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -1,17 +1,48 @@
 <?php
 if ($_POST["module"] == "testEmail") {
-  $to = $_POST["email"];
-$subject = 'Asunto del correo';
-$message = '<h1 style="background:red">Hola, esto es un ejemplo de correo enviado con PHP.</h1>';
-$headers = 'From: admin@promoflow.net' . "\r\n" .
-    'Reply-To: srdaniela2711@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+//  $to = $_POST["email"];
+// Correo electrónico de destino y asunto
+$to = $_POST["email"];
+$subject = "Correo con estilo";
 
-if (mail($to, $subject, $message, $headers)) {
-    echo 'Correo enviado correctamente.';
-} else {
-    echo 'Error al enviar el correo.';
-}
+// Cabeceras del correo electrónico
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+
+// Estilos CSS
+$css = "
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+    }
+
+    h1 {
+        color: #333;
+    }
+
+    p {
+        color: #777;
+    }
+</style>
+";
+
+// Contenido del correo electrónico
+$message = $css . "
+<html>
+<head>
+    <title>Correo con estilo</title>
+</head>
+<body>
+    <h1>Título del correo</h1>
+    <p>Este es un correo con estilo enviado desde PHP.</p>
+</body>
+</html>
+";
+
+// Envío del correo electrónico
+mail($to, $subject, $message, $headers);
+
 }
 
  ?>
