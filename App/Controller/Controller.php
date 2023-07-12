8235e28fd613a05de732346193f88ef5837c8896
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-12 14:50:55 -0500

    Update Controller.php
    ..ghfgh

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -38,8 +38,8 @@ $message = $css . "
       <title>Correo con estilo</title>
   </head>
   <body>
-  <div class='bacground' style= 'position: relative; width: 100%; background: purple;' >
-    <div class='header' style= 'width: 300px; left: 50%; transform: translateX(-50%); position: relative;'>
+  <div class='bacground' style= 'position: relative; width: 100%; background: rgb(252,252,252);' >
+    <div class='header' style= 'width: 300px; background: red; left: 50%; transform: translateX(-50%); position: relative;'>
       <img src='https://lanyardsforyou.com/Pages/General/Menu/Logo.png' alt='Imagen' style='display: block; margin-left: 15px; margin-top: 15px; width: 120px;'>
 
     </div>
