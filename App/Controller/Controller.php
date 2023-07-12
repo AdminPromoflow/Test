a3f7f9d38cd6f13fc5ab20c51aac11acf435b8ae
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-11 19:24:32 -0500

    Update Controller.php
    klj

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -1,6 +1,17 @@
 <?php
 if ($_POST["module"] == "testEmail") {
-  echo $_POST["email"] ;
+  $to = 'lonsus30@gmail.com';
+$subject = 'Asunto del correo';
+$message = 'Hola, esto es un ejemplo de correo enviado con PHP.';
+$headers = 'From: admin@promoflow.net' . "\r\n" .
+    'Reply-To: srdaniela2711@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if (mail($to, $subject, $message, $headers)) {
+    echo 'Correo enviado correctamente.';
+} else {
+    echo 'Error al enviar el correo.';
+}
 }
 
  ?>
