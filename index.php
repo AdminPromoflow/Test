c8a3a3ca796a6fc95c1c72d3118e9242a6db9d68
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-13 14:39:00 -0500

    Update index.php
    ghtf

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <button id="send" type="button" name="button">Enviar</button>
     </section>
     <div class='background' style= 'position: relative; width: 100%; background: rgb(245,245,245); ' >
-      <div class='background2' style='width: 60%; min-width: 30px; margin: 0 auto; position: relative; background: white;'>
+      <div class='background2' style='width: 60%; overflow-x: hidden; min-width: 30px; margin: 0 auto; position: relative; background: white;'>
 
 
       <div class='header' style= 'position: relative; background: rgb(63,152,237); background: linear-gradient(90deg, rgba(63,152,237,0.9) 0%, rgba(69,79,177,0.9) 53%, rgba(69,79,177,0.9) 75%, rgb(196, 56, 149) 100%);'>
@@ -29,6 +29,7 @@
       </div>
         </div>
     </div>
+
 
     <script type="text/javascript">
       var email = document.getElementById('email');
