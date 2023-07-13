2c367ad2efe1ac4a5050b48738097565506c0ab1
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-13 15:55:00 -0500

    Update Controller.php
    dgf

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -41,8 +41,8 @@ $message = $css . "
 
     </div>
     <div class='titleContainer' style='background: white; position: relative; width: 85%; background: blue; margin: 0 auto;' >
-    <h1 style='font-family: Oswald, sans-serif; font-size: calc(1.3vw + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: #664A99; margin-top: 1.2vw; text-align: center; '>HELLO AND WELCOME TO LANYARDS FOR YOU</h1>
-    <h2 style='font-family: Oswald, sans-serif; font-size: calc(0.9vw + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: #664A99; margin-top: 1vw; text-align: center; '>We're glad to have you here</h2>
+    <h1 style='font-family: Oswald, sans-serif; font-size: calc(1.3% + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: #664A99; margin-top: 1.2vw; text-align: center; '>HELLO AND WELCOME TO LANYARDS FOR YOU</h1>
+    <h2 style='font-family: Oswald, sans-serif; font-size: calc(0.9% + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: #664A99; margin-top: 1vw; text-align: center; '>We're glad to have you here</h2>
 
     </div>
       </div>
