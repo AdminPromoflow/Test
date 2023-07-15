7c1791415caffee49f9cdb39c9a2b774e24acf48
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-15 17:18:18 -0500

    Update Controller.php

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -65,7 +65,7 @@ if ($_POST["module"] == "testEmail") {
         <h3 style='font-family: Oswald, sans-serif; font-weight: 300; font-size: calc(1vw + 0.6em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center; margin-top: calc(0.4vw + 0.4em); '>www.lanyardsforyou.com</h3>
       </div>
 
-      <div class='header' style= 'position: relative; background: #555FA8; width: 100%; margin-top: calc(1.4vw + 0.4em);'>
+      <div class='footer' style= 'position: relative; background: #555FA8; width: 100%; margin-top: calc(1.4vw + 0.4em); heigth: calc(7.8vw + 5.9em); '>
         <img  alt='Imagen' style='display: block; width: calc(4.8vw + 2.9em); position: absolute; top: 1vw; left: 1vw; padding: 1vw 0;' src='https://lanyardsforyou.com/Pages/General/Menu/Logo.png'>
       </div>
 
