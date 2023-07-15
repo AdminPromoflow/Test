d56f50c2c16f296925bd537928e970e1e117f111
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-15 15:48:44 -0500

    Update Controller.php

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -58,6 +58,12 @@ if ($_POST["module"] == "testEmail") {
       <div class='titleContainer' style=' position: relative; width: 85%;  margin: 0 auto; margin-top: calc(1.4vw + 0.4em); ' >
         <h3 style='font-family: Oswald, sans-serif; font-weight: 500; font-size: calc(1vw + 0.6em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center;  '>We welcome you to our community</h3>
         <h4 style='font-family: Oswald, sans-serif; font-weight: 300; font-size: calc(0.8vw + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center;  '>Please make a note of your account info to access:</h4>
+
+        <h4 style='font-family: Oswald, sans-serif; font-weight: 300; font-size: calc(0.8vw + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center;  '>username: XXXXX</h4>
+        <h4 style='font-family: Oswald, sans-serif; font-weight: 300; font-size: calc(0.8vw + 0.4em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center;  '>password: XXXXX </h4>
+      <br>
+        <h3 style='font-family: Oswald, sans-serif; font-weight: 500; font-size: calc(1vw + 0.6em); position: relative; margin: 0 auto; letter-spacing: 0px; color: black; text-align: center;  '>www.lanyardsforyou.com</h3>
+
       </div>
 
 
